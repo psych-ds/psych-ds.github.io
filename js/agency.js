70bdd1bbca539eb0c9b531d8f237e39e1da4ea9a
Brian Leonard <brianleonard@Brians-MBP.hsd1.md.comcast.net>
2024-12-12 14:55:55 -0500

    updated website according to melissa's suggestions

--- js/agency.js
+++ js/agency.js
@@ -5,20 +5,15 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$('a.page-scroll').on('click', function(event) {
+    event.preventDefault();
+    var $anchor = $(this);
+    var navbarHeight = $('.navbar').outerHeight();
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - navbarHeight
+    }, 1500, 'easeInOutExpo');
 });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
